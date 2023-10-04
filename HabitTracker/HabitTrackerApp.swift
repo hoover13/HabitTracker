3d9d012d332e702447ad5666d2d065c08c9a02d5
hoover13 <hoovermtk25@gmail.com>
2023-10-04 01:58:37 -0400

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Min Thu Khine on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
